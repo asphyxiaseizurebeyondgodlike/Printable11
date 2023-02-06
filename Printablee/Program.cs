8465e492afcd5f0d89d2a3e306df1432ff24d918
kenshi <kenshi@WIN-5UDT5MPTTQM>
2023-02-06 23:05:06 +0300

    Добавьте файлы проекта.

---
+++ Printablee/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface Printable 
+{
+    void Print();
+}
+    
+class Book : Printable
+{
+    public void Print()
+    {
+        static void printBooks(Printable[] printables)
+        {
+            foreach (Printable Book in printables)
+            {
+                Console.WriteLine(Book);
+            }
+        }
+    }
+}
+
+class Magazine : Printable
+{
+    public void Print()
+    {
+        static void printBooks(Printable[] printables)
+        {
+            foreach (Printable Magazine in printables)
+            {
+                Console.WriteLine(Magazine + "QWE");
+            }
+        }
+
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Printable[] booksAndMagazines = { new Book(), new Magazine() };
+        foreach (Printable i in booksAndMagazines)
+        {
+            i.Print();
+        }
+    }
+}
+    
+
